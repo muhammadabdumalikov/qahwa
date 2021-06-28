77ed4a57ae5acaeadb2f18e4804831c4334f3384
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-06-28 15:56:33 +0500

    Add files

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 5151),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2179ddde57f389fdc5c3a287aafe9175'),
+      secret: env("ADMIN_JWT_SECRET", "2179ddde57f389fdc5c3a287aafe9175"),
     },
   },
 });
